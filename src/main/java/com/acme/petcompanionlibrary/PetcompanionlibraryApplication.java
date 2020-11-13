7a3d7795724467cc4c532a843e8bdc0f4f72db9b
Arix69sex <arix69sex@gmail.com>
2020-11-12 23:55:46 -0500

    Base Project

---
+++ src/main/java/com/acme/petcompanionlibrary/PetcompanionlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.acme.petcompanionlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcompanionlibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetcompanionlibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/petcompanionlibrary/PetcompanionlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.petcompanionlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcompanionlibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
